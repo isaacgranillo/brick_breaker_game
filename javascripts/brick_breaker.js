df985dacec06a19ad85dcefbaba67bb85becf53a
Isaac Granillo <isaac.granillo@gmail.com>
2016-04-13 13:07:08 -0700

    added movement and wall collision

--- javascripts/brick_breaker.js
+++ javascripts/brick_breaker.js
@@ -1,20 +1,47 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#1373a3";
-ctx.fill();
-ctx.closePath();
+function drawBall(){
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = "#b48484";
+	ctx.fill();
+	ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "#a0e082";
-ctx.fill();
-ctx.closePath();
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+		dx = -dx;
+	};
+	if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+		dy = -dy
+	};
+	x += dx;
+	y += dy;
+}
+setInterval(draw, 10);
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(180, 132, 132, 0.5)";
-ctx.stroke();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#1373a3";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "#a0e082";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "rgba(180, 132, 132, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
