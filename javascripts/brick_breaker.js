69e71bc0a64bf726c6a5d27578fa10e25f871d20
Isaac Granillo <isaac.granillo@gmail.com>
2016-04-13 14:55:57 -0700

    added brick collision

--- javascripts/brick_breaker.js
+++ javascripts/brick_breaker.js
@@ -22,10 +22,11 @@ var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
 var bricks = [];
+
 for(c = 0; c < brickColumnCount; c++){
 	bricks[c] = [];
 	for(r = 0; r < brickRowCount; r++){
-		bricks[c][r] = { x: 0, y: 0}
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
 	}
 }
 
@@ -50,6 +51,20 @@ function keyUpHandler(event){
 	}
 }
 
+function brickCollision(){
+	for(c = 0; c < brickColumnCount; c++){
+		for(r = 0; r < brickRowCount; r++){
+			var b = bricks[c][r];
+			if(b.status == 1){
+				if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+					dy = -dy;
+					b.status = 0;
+				}
+			}
+		}
+	}
+}
+
 function drawBall(){
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -69,15 +84,17 @@ function drawPaddle(){
 function drawBricks(){
 	for(c = 0; c < brickColumnCount; c++){
 		for(r = 0; r < brickRowCount; r++){
-			var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
-			var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "#b48484";
-			ctx.fill();
-			ctx.closePath();
+			if(bricks[c][r].status == 1){
+				var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+				var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#b48484";
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
@@ -87,6 +104,7 @@ function drawAll(){
 	drawBricks();
 	drawBall();
 	drawPaddle();
+	brickCollision();
 
 	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
 		dx = -dx;
